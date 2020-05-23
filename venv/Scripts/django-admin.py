99a6f41468aa268b01ea12598adc7f71a808c3f4
Eric <eschilha@gmail.com>
2020-05-22 21:21:43 -0400

    Uploading virtualenv files (requirements.txt and git hooks later)

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Temp Onedrive\OneDrive - University of Waterloo\Python\FHSearch Web\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
